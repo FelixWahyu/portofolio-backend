a8e75e47998ac741c3d81932e4f891a7ed1934e4
FelixWahyu <felixwahyusejati89@gmail.com>
2026-02-20 14:31:13 +0700

    Update fix cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import wakatimeRoute from "./routes/wakatimeRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:8080", "https://www.felixws.my.id"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
