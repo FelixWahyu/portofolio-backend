a2b50e5416f4f2815aaaf1f474e55bb3e57119c5
FelixWahyu <felixwahyusejati89@gmail.com>
2026-02-21 13:21:09 +0700

    Update configurasi untuk render

--- src/services/wakatimeService.js
+++ src/services/wakatimeService.js
@@ -2,19 +2,23 @@
 import { env } from "../config/env.js";
 
 export const fetchWakaTimeStats = async () => {
-  console.log("API KEY:", env.WAKATIME_API_KEY ? "exists" : "MISSING");
-  console.log("Fetching wakatime...");
+  // console.log("API KEY:", env.WAKATIME_API_KEY ? "exists" : "MISSING");
+  // console.log("Fetching wakatime...");
   const controller = new AbortController();
   const timeout = setTimeout(() => controller.abort(), 8000);
 
   try {
+    if (!env.WAKATIME_API_KEY) {
+      throw new Error("WAKATIME_API_KEY is missing");
+    }
+
     const response = await fetch("https://wakatime.com/api/v1/users/current/stats/last_7_days", {
       headers: {
         Authorization: "Basic " + Buffer.from(`${env.WAKATIME_API_KEY}:`).toString("base64"),
       },
       signal: controller.signal,
     });
-    console.log("Response status:", response.status);
+    // console.log("Response status:", response.status);
 
     const result = await response.json();
 
@@ -24,7 +28,8 @@ export const fetchWakaTimeStats = async () => {
 
     return result.data;
   } catch (err) {
-    console.log("Error:", err.message);
+    console.log("Error wakatime:", err.message);
+    throw err;
   } finally {
     clearTimeout(timeout);
   }
